4e04c6a8d96f0fbce77f374294e44012be519482
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-04-10 13:55:53 +0100

    added pop up when cart is empty

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let counter = 0;
 let display = document.querySelector('.text_content')
 let increment = document.querySelector('.increment')
 let decrement = document.querySelector('.decrement')
+let button = document.querySelector('.button')
+let pop_up = document.querySelector('.pop_up')
 
+
+// Add increment and decrement function
 function incrementBtn(){
     counter++;
     display.textContent = counter;
@@ -17,3 +21,18 @@ function decrementBtn(){
     display.textContent = counter;
 }
 decrement.addEventListener('click', decrementBtn)
+
+
+// Add pop_up when cart is empty
+function addCart(){
+    if (counter === 0){
+       pop_up.classList.add('active')
+       setTimeout(() => {
+        pop_up.classList.remove('active');
+      }, 3000);
+    }
+    else{
+        pop_up.classList.remove('active')
+    }
+}
+button.addEventListener('click', addCart)
