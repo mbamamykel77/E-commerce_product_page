251e50369cbdcdf204749925f5e8cd73abcf5f63
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-04-13 03:47:02 +0100

    added functionality for the cart

--- index.js
+++ index.js
@@ -1,58 +1,110 @@
 let counter = 0;
 
-let display = document.querySelector('.text_content')
-let increment = document.querySelector('.increment')
-let decrement = document.querySelector('.decrement')
-let button = document.querySelector('.button')
-let pop_up = document.querySelector('.pop_up')
-let images = document.querySelectorAll('.images')
-let mainImg = document.querySelector('.box_image')
+let display = document.querySelector(".text_content");
+let increment = document.querySelector(".increment");
+let decrement = document.querySelector(".decrement");
+let button = document.querySelector(".button");
+let pop_up = document.querySelector(".pop_up");
+let images = document.querySelectorAll(".images");
+let mainImg = document.querySelector(".box_image");
+let cart = document.querySelector(".cart");
+let filledCart = document.querySelector(".filled");
+let itemCount = document.querySelector(".item_count");
+let totalAmount = document.querySelector(".total");
+let span = document.querySelector("#span");
+let deleteCart = document.querySelector(".delete");
+let checkOut = document.querySelector(".filled_button");
 
+// Delete cart
+function clearCart() {
+  span.style.display = "none";
+  filledCart.classList.remove("active1");
+  filledCart.style.marginTop = "-380px";
+  pop_up.classList.add("active");
+  setTimeout(() => {
+    pop_up.classList.remove("active");
+  }, 3000);
+}
+deleteCart.addEventListener("click", clearCart);
 
 // Add increment
-function incrementBtn(){
-    counter++;
-    display.textContent = counter;
+function incrementBtn() {
+  counter++;
+  display.textContent = counter;
+  totalAmount.style.marginLeft = "0.5rem";
+  totalAmount.style.color = "black";
 }
-increment.addEventListener('click', incrementBtn)
+increment.addEventListener("click", incrementBtn);
 
 // Add decrement
-function decrementBtn(){
+function decrementBtn() {
+  if (counter !== 0) {
+    counter--;
+  }
+  display.textContent = counter;
+  totalAmount.style.marginLeft = "0.5rem";
+  totalAmount.style.color = "black";
+  if (counter == 0) {
+    span.style.display = "none";
+  }
+}
+decrement.addEventListener("click", decrementBtn);
+
+// Action when cart is empty
+function addCart() {
+  if (counter === 0) {
+    itemCount.textContent = counter;
+    let amount = 125 * Number(itemCount.textContent);
+    totalAmount.textContent = "$ " + amount + ".00";
+    pop_up.classList.add("active");
+    filledCart.classList.remove("active1");
+    filledCart.style.marginTop = "-380px";
+    setTimeout(() => {
+      pop_up.classList.remove("active");
+    }, 3000);
+    // decrease cart content with item and init picked
     if (counter !== 0) {
-        counter--;
+      counter--;
+      itemCount.textContent = counter;
+      let amount = 125 * Number(itemCount.textContent);
+      totalAmount.textContent = "$ " + amount + ".00";
     }
-    display.textContent = counter;
+    // increase cart content with item and unit picked
+  } else if (counter > 0) {
+    itemCount.textContent = counter;
+    let amount = 125 * Number(itemCount.textContent);
+    totalAmount.textContent = "$ " + amount + ".00";
+    span.style.display = "block";
+    span.textContent = counter;
+  }
 }
-decrement.addEventListener('click', decrementBtn)
+button.addEventListener("click", addCart);
 
-
-// Add pop_up when cart is empty
-function addCart(){
-    if (counter === 0){
-       pop_up.classList.add('active')
-       setTimeout(() => {
-        pop_up.classList.remove('active');
-      }, 3000);
-    }
-    else{
-        pop_up.classList.remove('active')
-    }
+// Add pop_up when cart is filled
+function cartFilled() {
+  filledCart.classList.add("active1");
+  filledCart.style.marginTop = "380px";
+//   if (counter == 0){
+//     filledCart.style.display = 'none'
+//   }
+//   if(counter > 0){
+//     filledCart.style.display = 'block'
+//   }
 }
-button.addEventListener('click', addCart)
-
+cart.addEventListener("click", cartFilled);
 
 // Looping through the image
- for (let i = 0; i < images.length; i++){
-    images[i].addEventListener('click', function(){
-        mainImg.src = this.src
-    })
- }
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function () {
+    mainImg.src = this.src;
+  });
+}
 
 //  menu bar
 function openNav() {
-    document.querySelector('.desktop_menu').style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.querySelector('.desktop_menu').style.width  = "0";
-  }
+  document.querySelector(".desktop_menu").style.width = "250px";
+}
+
+function closeNav() {
+  document.querySelector(".desktop_menu").style.width = "0";
+}
